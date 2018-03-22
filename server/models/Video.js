b1dc4e202f1475daddba0d52006a74355cd5a6fe
Muhammad Usama <usama6000@gmail.com>
2018-03-22 14:53:13 +0500

    all functions are done for video controller in promise

--- server/models/Video.js
+++ server/models/Video.js
@@ -1,3 +1,12 @@
+import AWS from 'aws-sdk';
+import formidable from 'formidable';
+import Pusher from 'pusher';
+import md5 from 'js-md5';
+import _ from 'lodash';
+import fs from 'fs';
+import transcoder from '../lib/helpers/transcoder';
+import db from './../models';
+
 export default (sequelize, DataTypes) => {
   const Video = sequelize.define('Video', {
     filename: DataTypes.STRING,
@@ -28,3 +37,174 @@ export default (sequelize, DataTypes) => {
 
   return Video;
 };
+
+exports.updateUserUploadingFalse = function(userId){
+
+  return new Promise(function(resolve, reject){
+    db.User.update({
+      isUploading: false,
+    }, {
+      where: {
+        id: userId,
+      },
+    }).then(function(){
+      resolve();
+    }).catch(function(err){
+      reject(err);
+    })
+  });
+}
+
+exports.destoryVideoById = function(videoId){
+  return new Promise(function(resolve, reject){
+    db.Video.destroy({ where: { id: videoId } })
+    .then(function(){
+      resolve();
+    })
+    .catch(function(userErr){
+      reject(userErr);
+    });
+  });
+}
+
+exports.updateVideoDelete = function(videoId){
+  return new Promise(function(resolve, reject){
+    db.Video.update({ isDeleted: true }, { where: { id: videoId } })
+          .then(function(){
+            resolve();
+          })
+          .catch(function(err3){
+            reject(err3);
+          });
+  });
+}
+
+exports.videoCreate = function(fileName, ChannelId, videoDuration){
+  return new Promise(function(resolve, reject){
+    db.Video.create({
+      filename: fileName,
+      title: fileName.split('/').reverse()[0].split('.')[0],
+      videoPath: '',
+      thumbnailPath: '',
+      desc: '',
+      channelId: ChannelId,
+      isDeleted: false,
+      accessibility: 1,
+      canLike: true,
+      canComment: true,
+      duration: videoDuration,
+    }).then(function(result){
+      resolve(result);
+    }).catch(function(err){
+      reject(err);
+    })
+  });
+}
+
+exports.updateUserVideoTrue = function(userId){
+  return new Promise(function(resolve, reject){
+    db.User.update({
+      isUploading: true,
+    }, {
+      where: {
+        id: userId,
+      },
+    }).then(function(){
+      resolve();
+    }).catch(function(err){
+      reject(err);
+    })
+  });
+}
+
+exports.updateVideo = function(videoId, userId, videoHash, videoName, suffix){
+  return new Promise(function(resolve, reject){
+    db.Video.update({
+      pathToOriginal: `${process.env.AWS_VIDEO_CDN}/user_${userId}/${videoHash}/${videoName}.${suffix}`,
+    }, {
+      where: { id: videoId },
+    }).then(function(){
+      resolve();
+    }).catch(function(err5){
+      reject(err5);
+      //console.log('FAILED updating video pathToOriginal in DB', err5);
+      //crashReport(res, req.user.id, videoId, videoHash, searchId);
+    });
+  });
+}
+
+exports.videoFindOne = function(videoId){
+  return new Promise(function(resolve, reject){
+    db.Video.findOne({ where: { id: videoId, isDeleted: false },
+    }).then(function(result2){
+      resolve(result2);
+    }).catch(function(err){
+      reject(err);
+    })
+  });
+}
+
+exports.updateVideoPath = function(videopaths, videoId){
+  return new Promise(function(resolve, reject){
+    db.Video.update({
+      ...videopaths,
+    }, {
+      where: { id: videoId, isDeleted: false },
+    }).then(function(){
+      resolve();
+    }).catch(function(err3){
+      reject(err3);
+    })
+  });
+}
+
+exports.deleteVideo = function(videoId){
+    return Promise(function(resolve, reject){
+      db.Video.update({ isDeleted: true }, { where: { id: videoId } })
+      .then(function(){
+        resolve();
+      }).catch(function(err){
+        reject(err);
+      })
+    });
+}
+
+exports.getVideoInfo = function(videoId){
+  const {id} = videoId;
+  return new Promise(function(resolve,reject){
+    db.Video.findOne({
+      where: { id },
+      include: [{
+        model: db.Tag,
+        through: {
+          model: db.VideoTag,
+          attributes: [],
+        },
+        attributes: ['id', 'name'],
+      }],
+    }).then(function(data){
+      const { title, desc, thumbnailPath, canLike, canComment, accessibility } = data;
+      const tags = data.Tags;
+      resolve({ title, desc, thumbnailPath, canLike, canComment, accessibility, tags });
+    }).catch(function(err){
+      reject(err);
+    })
+  });
+  // const { id } = req.params;
+  // db.Video.findOne({
+  //   where: { id },
+  //   include: [{
+  //     model: db.Tag,
+  //     through: {
+  //       model: db.VideoTag,
+  //       attributes: [],
+  //     },
+  //     attributes: ['id', 'name'],
+  //   }],
+  // })
+  // .then((data) => {
+  //   const { title, desc, thumbnailPath, canLike, canComment, accessibility } = data;
+  //   const tags = data.Tags;
+  //   res.status(200).json({ title, desc, thumbnailPath, canLike, canComment, accessibility, tags });
+  // }).catch(err => res.status(500).json(err));
+}
