1aa5429cc089c118ca810cf96cd414c16e7bf593
Muhammad Usama <usama6000@gmail.com>
2018-03-26 18:06:31 +0500

    26-march-2018 subscription refactoring is completed and working

--- server/controllers/subscriptionController.js
+++ server/controllers/subscriptionController.js
@@ -1,84 +1,25 @@
 import _ from 'lodash';
 import db from './../models';
 import { channelIndex } from './../config/algolia';
-
+const subscriptionModel = require('./../models/UserSubscription');
 const subscriptionController = {};
 
 subscriptionController.getAllSubscription = (req, res) => {
-  const { id } = req.user;
-
-  db.UserSubscription.findAll({
-    where: { userId: id },
-    group: ['UserSubscription.id', 'Channel.id', 'Channel.Videos.id', 'Channel.User.id'],
-    order: [
-      ['createdAt', 'ASC'],
-    ],
-    include: [
-      {
-        model: db.Channel,
-        attributes: ['id', 'name', 'desc', 'userId', 'searchId', 'color', [db.sequelize.fn('COUNT', db.sequelize.col('Channel.UserSubscriptions.id')), 'subscribers']],
-        include: [{
-          model: db.UserSubscription,
-          attributes: [],
-        }, {
-          model: db.Video,
-          attributes: ['id'],
-        }, {
-          model: db.User,
-          attributes: ['id', 'avatarPath'],
-        }],
-      },
-    ],
-  }).then((subscriptions) => {
-    return res.status(200).json({
-      subscriptions,
-      success: true,
-    });
-  }).catch((err) => {
-    return res.status(501).json({
-      err,
-    });
-  });
+  subscriptionModel.getAllByUserId(req.user)
+  .then(function(data){
+    return res.status(200).json(data);
+  }).catch(function(err){
+    return res.status(501).json(err);
+  })
 };
 
 subscriptionController.getSubscription = (req, res) => {
-  const { channelId } = req.params;
-  if (req.user) {
-    const { id } = req.user;
-    db.UserSubscription.findOne({
-      where: {
-        userId: id,
-        channelId,
-      },
-    }).then((userSub) => {
-      db.UserSubscription.count({
-        where: { channelId },
-      }).then((count) => {
-        return res.status(200).json({
-          subscribed: !_.isEmpty(userSub),
-          totalSubscriber: count,
-          success: true,
-        });
-      });
-    }).catch((err) => {
-      return res.status(500).json({
-        err,
-      });
-    });
-  } else {
-    db.UserSubscription.count({
-      where: { channelId },
-    }).then((count) => {
-      return res.status(200).json({
-        subscribed: false,
-        totalSubscriber: count,
-      });
-    }).catch((err) => {
-      return res.status(500).json({
-        err,
-      });
-    });
-  }
+  subscriptionModel.getUserSubscription(req.params, req.user)
+  .then(function(data){
+    return res.status(200).json(data);
+  }).catch(function(err){
+    return res.status(500).json(err);
+  })
 };
 
 subscriptionController.newSubscription = (req, res) => {
@@ -86,32 +27,27 @@ subscriptionController.newSubscription = (req, res) => {
 
   if (req.user) {
     const { id } = req.user;
-    db.UserSubscription.create({ userId: id, channelId }, {
-      where: {
-        channelId,
-        userId: id,
-      },
-    }).then((userSub) => {
-      // INCREMENT CHANNEL SUBSCRIBER IN SEARCH INDEX
-      if (searchId && process.env.NODE_ENV === 'production') {
-        channelIndex.partialUpdateObject({
-          subscribers: {
-            value: 1,
-            _operation: 'Increment',
-          },
-          objectID: searchId,
-        }, (err, content) => {
-          if (err) {
-            console.error(err);
-          }
-          console.log('Channel Subscriber Incremented');
-        });
-      }
-
-      return res.status(200).json({ userSub, success: true });
-    }).catch((err) => {
-      return res.status(500).json({ err });
-    });
+    subscriptionModel.createSubscription(channelId, id)
+    .then(function(userSub){
+        // INCREMENT CHANNEL SUBSCRIBER IN SEARCH INDEX
+        if (searchId && process.env.NODE_ENV === 'production') {
+          channelIndex.partialUpdateObject({
+            subscribers: {
+              value: 1,
+              _operation: 'Increment',
+            },
+            objectID: searchId,
+          }, (err, content) => {
+            if (err) {
+              console.error(err);
+            }
+            console.log('Channel Subscriber Incremented');
+          });
+        }
+      return res.status(200).json(userSub);
+    }).catch(function(err){
+      return res.status(500).json( err );
+    })
   } else {
     return res.status(401).json({
       login: false,
@@ -123,15 +59,10 @@ subscriptionController.newSubscription = (req, res) => {
 subscriptionController.deleteSubscription = (req, res) => {
   const { channelId, searchId } = req.body;
 
-  if (req.user) {
+  if(req.user){
     const { id } = req.user;
-    db.UserSubscription.destroy({
-      where: {
-        channelId,
-        userId: id,
-      },
-    }).then((userSub) => {
-      // DECREMENT CHANNEL SUBSCRIBER IN SEARCH INDEX
+    subscriptionModel.removeSubscription(channelId, id)
+    .then(function(userSub){
       if (searchId && process.env.NODE_ENV === 'production') {
         channelIndex.partialUpdateObject({
           subscribers: {
@@ -148,10 +79,10 @@ subscriptionController.deleteSubscription = (req, res) => {
       }
 
       return res.status(200).json({ userSub, success: true });
-    }).catch((err) => {
+    }).catch(function(err){
       return res.status(500).json({ err });
-    });
-  } else {
+    })
+  }else{
     return res.status(401).json({
       login: false,
       message: 'User is not logged in.',
--- server/models/UserSubscription.js
+++ server/models/UserSubscription.js
@@ -1,3 +1,5 @@
+import db from './../models';
+import _ from 'lodash';
 export default (sequelize, DataTypes) => {
   const UserSubscription = sequelize.define('UserSubscription', {
     id: {
@@ -9,3 +11,109 @@ export default (sequelize, DataTypes) => {
 
   return UserSubscription;
 };
+
+exports.getAllByUserId = function(userId){
+  const { id } = userId;
+
+  return new Promise(function(resolve, reject){
+    db.UserSubscription.findAll({
+      where: { userId: id },
+      group: ['UserSubscription.id', 'Channel.id', 'Channel.Videos.id', 'Channel.User.id'],
+      order: [
+        ['createdAt', 'ASC'],
+      ],
+      include: [
+        {
+          model: db.Channel,
+          attributes: ['id', 'name', 'desc', 'userId', 'searchId', 'color', [db.sequelize.fn('COUNT', db.sequelize.col('Channel.UserSubscriptions.id')), 'subscribers']],
+          include: [{
+            model: db.UserSubscription,
+            attributes: [],
+          }, {
+            model: db.Video,
+            attributes: ['id'],
+          }, {
+            model: db.User,
+            attributes: ['id', 'avatarPath'],
+          }],
+        },
+      ],
+    }).then(function(subscriptions){
+      resolve({subscriptions, success:true,})
+    }).catch(function(err){
+      reject({err});
+    })
+  });
+}
+
+exports.getUserSubscription = function(channelid, userId){
+  const { channelId } = channelid;
+  return new Promise(function(resolve, reject){
+  if (userId) {
+    const { id } = userId;
+    db.UserSubscription.findOne({
+      where: {
+        userId: id,
+        channelId,
+      },
+    }).then((userSub) => {
+      db.UserSubscription.count({
+        where: { channelId },
+      }).then((count) => {
+        var data = {
+          subscribed: !_.isEmpty(userSub),
+          totalSubscriber: count,
+          success: true,
+        }
+        resolve(data);
+      });
+    }).catch((err) => {
+      reject({err});
+    });
+  } else {
+    db.UserSubscription.count({
+      where: { channelId },
+    }).then((count) => {
+      var data = {
+        subscribed: false,
+        totalSubscriber: count,
+      }
+      resolve(data);
+    }).catch((err) => {
+      reject({err});
+    });
+  }
+  });
+}
+
+exports.createSubscription = function(channelId, userId){
+  return new Promise(function(resolve, reject){
+    db.UserSubscription.create(
+      { userId: userId, channelId 
+      }, {
+      where: {
+        channelId,
+        userId: userId,
+      },
+    }).then(function(userSub){
+      resolve(userSub);
+    }).catch(function(err){
+      reject({err})
+    });
+  });
+}
+
+exports.removeSubscription = function(channelId, userId){
+  return new Promise(function(resolve, reject){
+    db.UserSubscription.destroy({
+      where: {
+        channelId,
+        userId: userId,
+      },
+    }).then(function(userSub){
+      resolve(userSub);
+    }).catch(function(err){
+      reject({err});
+    })
+  });
+}
