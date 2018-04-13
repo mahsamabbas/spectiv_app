ca36e8412ac6ef23ba17c16a5a1685dd6b1619ac
Muhammad Usama <usama6000@gmail.com>
2018-04-13 16:19:49 +0500

    13-april-2018 log folders and files are dynamic from env

--- server/config/logging.js
+++ server/config/logging.js
@@ -1,17 +1,20 @@
 var winston = require('winston');
 const fs = require('fs');
-const logDir = 'log';
+const logDiractory = process.env.LOGS_DIRECTORY;
+const errFile = process.env.ERROR_FILE;
+const infoFile = process.env.INFO_FILE;
 
+console.log(infoFile);
 
 exports.saveErrorLog = function(message){
-    if (!fs.existsSync(logDir)) {
-        fs.mkdirSync(logDir);
+    if (!fs.existsSync(logDiractory)) {
+        fs.mkdirSync(logDiractory);
       }
     const tsFormat = () => (new Date()).toLocaleTimeString();
     const logger = new (winston.Logger)({
       transports: [
         new (winston.transports.File)({
-          filename: `${logDir}/errorLogs.log`,
+          filename: `${logDiractory}/`+errFile,
           timestamp: tsFormat,
           level: 'error'
         })
@@ -21,14 +24,14 @@ exports.saveErrorLog = function(message){
   }
 
   exports.saveInfoLog = function(message){
-    if (!fs.existsSync(logDir)) {
-        fs.mkdirSync(logDir);
+    if (!fs.existsSync(logDiractory)) {
+        fs.mkdirSync(logDiractory);
       }
     const tsFormat = () => (new Date()).toLocaleTimeString();
     const logger = new (winston.Logger)({
       transports: [
         new (winston.transports.File)({
-          filename: `${logDir}/infoLogs.log`,
+          filename: `${logDiractory}/`+infoFile,
           timestamp: tsFormat,
           level: 'info'
         })
