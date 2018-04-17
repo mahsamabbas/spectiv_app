b9c9e683ba25ec014b6b5cea3bd9ae36e47ca5dc
Muhammad Usama <usama6000@gmail.com>
2018-04-17 16:48:57 +0500

    added LOGGING option true/false for logging in env

--- server/config/logging.js
+++ server/config/logging.js
@@ -1,18 +1,22 @@
 var winston = require('winston');
 const fs = require('fs');
+const logs = process.env.LOGGING;
 const logDirectory = process.env.LOGS_DIRECTORY;
 const errFile = process.env.ERROR_FILE;
 const infoFile = process.env.INFO_FILE;
+const warningFile = process.env.WARNING_FILE;
+const debugFile = process.env.DEBUG_FILE;
 
-exports.saveErrorLog = function(message){
+exports.saveErrorLog = function (message) {
+  if (logs == "true") {
     if (!fs.existsSync(logDirectory)) {
-        fs.mkdirSync(logDirectory);
-      }
+      fs.mkdirSync(logDirectory);
+    }
     const tsFormat = () => (new Date()).toLocaleTimeString();
     const logger = new (winston.Logger)({
       transports: [
         new (winston.transports.File)({
-          filename: `${logDirectory}/`+errFile,
+          filename: `${logDirectory}/` + errFile,
           timestamp: tsFormat,
           level: 'error'
         })
@@ -20,16 +24,18 @@ exports.saveErrorLog = function(message){
     });
     logger.error(message);
   }
+}
 
-  exports.saveInfoLog = function(message){
+exports.saveInfoLog = function (message) {
+  if (logs == "true") {
     if (!fs.existsSync(logDirectory)) {
-        fs.mkdirSync(logDirectory);
-      }
+      fs.mkdirSync(logDirectory);
+    }
     const tsFormat = () => (new Date()).toLocaleTimeString();
     const logger = new (winston.Logger)({
       transports: [
         new (winston.transports.File)({
-          filename: `${logDirectory}/`+infoFile,
+          filename: `${logDirectory}/` + infoFile,
           timestamp: tsFormat,
           level: 'info'
         })
@@ -37,4 +43,42 @@ exports.saveErrorLog = function(message){
     });
     logger.info(message);
   }
-  
+}
+
+exports.saveWarningLog = function (message) {
+  if (logs == "true") {
+    if (!fs.existsSync(logDirectory)) {
+      fs.mkdirSync(logDirectory);
+    }
+    const tsFormat = () => (new Date()).toLocaleTimeString();
+    const logger = new (winston.Logger)({
+      transports: [
+        new (winston.transports.File)({
+          filename: `${logDirectory}/` + warningFile,
+          timestamp: tsFormat,
+          level: 'warn'
+        })
+      ]
+    });
+    logger.warn(message);
+  }
+}
+
+exports.saveDebugLog = function (message) {
+  if (logs == "true") {
+    if (!fs.existsSync(logDirectory)) {
+      fs.mkdirSync(logDirectory);
+    }
+    const tsFormat = () => (new Date()).toLocaleTimeString();
+    const logger = new (winston.Logger)({
+      transports: [
+        new (winston.transports.File)({
+          filename: `${logDirectory}/` + debugFile,
+          timestamp: tsFormat,
+          level: 'debug'
+        })
+      ]
+    });
+    logger.debug(message);
+  }
+}
