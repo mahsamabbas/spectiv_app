b05f5544d2ac78ff8ff0f6bcf7337af5ff347080
WINDOWS-OBN5CBG\Mahsam <syedmahsam@gmail.com>
2018-04-18 14:50:02 +0500

    latest code - 18th April

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ export default {
   entry: [
     'webpack-hot-middleware/client?reload=true/__webpack_hmr',
     path.join(__dirname, 'client/main.js'),
+
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
@@ -19,20 +20,40 @@ export default {
       inject: 'body',
       filename: 'index.html',
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
+    //new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    //new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify('dev'),
       DEBUG: JSON.stringify(process.env.DEBUG),
       AWS_VIDEO_CDN: JSON.stringify(process.env.AWS_VIDEO_CDN),
     }),
   ],
-  resolve: {
-    extensions: ['', '.js', '.json'],
-  },
+  // resolve: {
+  //   extensions: ['', '.js', '.json'],
+  // },
   module: {
-    loaders: [
+     loaders: [
+       {
+         test: /\.jsx?$/,
+         exclude: /node_modules/,
+         loader: 'babel-loader',
+         query: {
+           presets: ['react', 'es2015'],
+         },
+       },
+       {
+         test: /\.scss$/,
+         loaders: ['style', 'css', 'sass'],
+         include: path.join(__dirname, 'client'),
+         exclude: /node_modules/,
+       },
+       {
+         test: /\.css$/,
+         loaders: ['style', 'css'],
+       },
+     ],
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -40,19 +61,14 @@ export default {
         query: {
           presets: ['react', 'es2015'],
         },
+        //use: ['react-hot-loader/webpack','babel-loader'],
+
       },
       {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass'],
+        test: /\.scss$/, exclude: /node_modules/,
         include: path.join(__dirname, 'client'),
-        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-      },
-    ],
-    rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
